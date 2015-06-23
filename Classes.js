2513952f19d6ce1da07a509641bdc39ef2c83bf6
amitthakkar <d0ntgetHyper>
2015-06-23 19:44:42 +0530

    Adding class+inheritance demo

---
+++ Classes.js
@@ -0,0 +1,33 @@
+/**
+ * Created by Amit Thakkar on 23/06/15.
+ */
+(function () {
+    "use strict";
+    class Person {
+        constructor(name) {
+            this.name = name;
+        }
+
+        getName() {
+            return this.name;
+        }
+    }
+    class Employee extends Person {
+        constructor(name, id) {
+            super(name);
+            this.id = id;
+        }
+
+        getId() {
+            return this.id;
+        }
+    }
+
+    let amit = new Employee('Amit Thakkar', 32);
+    let namita = new Employee('Namita Malik', 45);
+
+    console.log(amit.getName()); // Amit Thakkar
+    console.log(amit.getId()); // 32
+    console.log(namita.getName()); // Namita Malik
+    console.log(namita.getId()); // 45
+})();
