3427224efac329727212aab38caa96d72a9fcd00
Amit Thakkar <vigildbest@gmail.com>
2016-03-28 17:49:07 +0530

    Making complete demo for arrow functions.

--- ArrowFunction.js
+++ ArrowFunction.js
@@ -9,25 +9,26 @@
      *
      * 2. Arrow functions cannot be used a constructors and will throw an error when used with new.
      *
-     * 3. The value of this inside of the function can’t be changed, it remains the same value throughout the entire
+     * 3. arguments inside the arrow function will be empty object, so overloading with arrow function can't be done.
+     *
+     * 4. The value of this inside of the function can’t be changed, it remains the same value throughout the entire
      * lifecycle of the function
      * */
-    /*var getANumber = () => {
+    var getANumber = () => {
         return 1;
     };
-    console.log("getANumber()", getANumber());*/
+    console.log("getANumber()", getANumber());
 
     var multiplyBy2 = n => n * 2;
     console.log("multiplyBy2(3)", multiplyBy2(3));
 
-    /*let add_1 = (n1, n2) =>  n1 + n2;
+    let add_1 = (n1, n2) =>  n1 + n2;
     console.log("add_1(2, 3)", add_1(2, 3));
 
     let add_2 = (n1, n2) => {
         let sum = n1 + n2;
-        conosle.log(arguments);
+        console.log(arguments);
         return sum;
     };
-    console.log("add_2(2, 3)", add_2(2, 3));*/
-
+    console.log("add_2(2, 3)", add_2(2, 3));
 })();
